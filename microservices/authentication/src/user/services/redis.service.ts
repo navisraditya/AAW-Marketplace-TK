010e3325d94bcd5c8488b493793d04300e7fd772
Naomi <gabriella.naomi21@ui.ac.id>
2025-05-16 01:48:34 +0700

    fix yaml file

--- microservices/authentication/src/user/services/redis.service.ts
+++ microservices/authentication/src/user/services/redis.service.ts
@@ -7,7 +7,10 @@ class RedisService {
 
   private constructor() {
     this.client = createClient({
-      url: process.env.REDIS_URL || 'redis://localhost:6379'
+      url: process.env.REDIS_URL || 'redis://localhost:6379',
+      socket: {
+        connectTimeout: 10000
+      }
     });
 
     this.client.on('error', (err) => {
